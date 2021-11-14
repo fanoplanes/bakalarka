22dbdff0578ae352cbf570201cdeb52fa6763194
fanoplanes <pontryagin@protonmail.com>
2021-11-14 23:56:43 +0100

    added an explicit way to get a plot of difference between effective theory and photonic structure

--- Subtract/koeficienty-TE-parallel-subtract.cpp
+++ Subtract/koeficienty-TE-parallel-subtract.cpp
@@ -31,10 +31,14 @@ using namespace boost::multiprecision;
 using namespace std;
 const FLOAT_TYPE eps_one = 1.;		// one  | vzorka |  air
 const FLOAT_TYPE mi_one = 1.;
+const FLOAT_TYPE eps_air = 1.;
+const FLOAT_TYPE mi_air = 1.;
+
 const COMPLEX_TYPE i  (0, 1);
 
 Eigen::Matrix<COMPLEX_TYPE, 2, 2> transfermatrix(FLOAT_TYPE, FLOAT_TYPE, FLOAT_TYPE, FLOAT_TYPE);
 Eigen::Matrix<COMPLEX_TYPE, 2, 2> intermatrix(FLOAT_TYPE, FLOAT_TYPE, FLOAT_TYPE, FLOAT_TYPE);
+FLOAT_TYPE numeric(FLOAT_TYPE, FLOAT_TYPE, FLOAT_TYPE, FLOAT_TYPE);
 
 int main()
 {
@@ -46,12 +50,12 @@ int main()
 	const FLOAT_TYPE delta = 1e-2;
 	const FLOAT_TYPE theta_delta = 1e-2;
 	const FLOAT_TYPE eps_a = 1.;
-	const FLOAT_TYPE eps_air = 1.;
 	const FLOAT_TYPE eps_b = 4.;
 	const FLOAT_TYPE mi_a = 1.;
 	const FLOAT_TYPE mi_b = 1.;
-	const FLOAT_TYPE mi_air = 1.;
 	const FLOAT_TYPE omega_0 = PI/(2*sqrt(eps_b)*l_b);
+	FLOAT_TYPE eps_eff_parr=0;
+	FLOAT_TYPE length;
 	FLOAT_TYPE structure[2*N][2];
 	unsigned long int thett;
 
@@ -77,6 +81,19 @@ int main()
 		structure[2*i +1][1] = l_a;
 	}
 
+	for(int i=0; i<2*N; i++)
+	{
+		eps_eff_parr+= structure[i][0]*structure[i][1];
+	}
+
+	length=0;
+
+	for(int i=0; i<2*N; i++)
+	{
+		length += structure[i][1];
+	}
+
+	eps_eff_parr/=length;
 
 	Eigen::Matrix<COMPLEX_TYPE, 2, 2> Out;
 
@@ -107,11 +124,11 @@ int main()
 			{
 			if(eps_one == eps_air)
 			{
-				fout << omega/omega_0 << "\t" << theta*180./PI << "\t" << T << endl;
+				fout << omega/omega_0 << "\t" << theta*180./PI << "\t" << abs(T-numeric(eps_eff_parr, omega, theta, length)) << endl;
 			}
 			else
 			{
-				fout << omega/omega_0 << "\t" << theta*180./PI << "\t" << 1 - R << endl;
+				fout << omega/omega_0 << "\t" << theta*180./PI << "\t" << abs((1 - R) - numeric(eps_eff_parr, omega, theta, length)) << endl;
 			}
 			}
 		}
@@ -143,4 +160,14 @@ Eigen::Matrix<COMPLEX_TYPE, 2, 2> intermatrix(FLOAT_TYPE permittivity, FLOAT_TYP
 	Out <<	exp(phi), 	0,
 			0, 		exp(-phi);
 	return Out;
+}
+FLOAT_TYPE numeric(FLOAT_TYPE eps_2, FLOAT_TYPE omega, FLOAT_TYPE theta, FLOAT_TYPE l)
+{
+	COMPLEX_TYPE k_1z = omega*sqrt(eps_one)*cos(theta);
+	FLOAT_TYPE inter_2 = eps_2 - eps_one*sin(theta)*sin(theta);
+	FLOAT_TYPE inter_3 = eps_air - eps_one*sin(theta)*sin(theta);
+	COMPLEX_TYPE k_2z = omega*sqrt(COMPLEX_TYPE (inter_2, 0.));
+	COMPLEX_TYPE k_3z = omega*sqrt(COMPLEX_TYPE(inter_3,0));
+	COMPLEX_TYPE r = - ((1. - (k_1z/k_3z))*cos(k_2z*l) - i * ((k_2z/k_3z)-(k_1z/k_2z))*sin(k_2z*l))/((1. + (k_1z/k_3z))*cos(k_2z*l) - i * ((k_2z/k_3z)+(k_1z/k_2z))*sin(k_2z*l));
+	return 1 - norm(r);
 }
